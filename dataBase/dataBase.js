6d83194e607a50153623a1f41047e74353d9c61e
sava11 <sovergonnew@gmail.com>
2025-05-07 14:22:00 +0300

    simple changes

--- dataBase/dataBase.js
+++ dataBase/dataBase.js
@@ -3,7 +3,7 @@ const pool = mysql.createPool({
     // connectionLimit: 5,
     waitForConnections:true,
     host: process.env.DB_HOST,
-    //port:process.env.DB_PORT,
+    port:process.env.DB_PORT,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD
